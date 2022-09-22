56e4df8b90205158370663e91ddbe53deb8207a9
januariogon <ric_januario@hotmail.com>
2022-09-21 21:54:35 -0300

    appearance status bar

--- app/src/main/java/co/tiagoaguiar/course/instagram/main/view/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/course/instagram/main/view/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowInsetsController
+import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
 import co.tiagoaguiar.course.instagram.R
 
@@ -17,5 +18,12 @@ class MainActivity : AppCompatActivity() {
                 WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS)
         }
         window.statusBarColor = ContextCompat.getColor(this, R.color.gray)
+
+        val toolbar = findViewById<Toolbar>(R.id.main_toolbar)
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_insta_camera)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = ""
     }
 }
